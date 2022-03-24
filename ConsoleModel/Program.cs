7cdf909d55d87814fd3ff89625a2f660940fa4f5
dvnkboi <aymandb@outlook.com>
2022-03-24 15:28:47 +0100

    upgrade to net5.0 remove unused libraries

--- ConsoleModel/Program.cs
+++ ConsoleModel/Program.cs
@@ -12,90 +12,63 @@ namespace ConsoleModel
             Connection.Connect("Server=127.0.0.1;Database=csharp;Uid=root;Pwd=8576;","MySql");
 
 
-            ////description of student table
-            //Dictionary<string, string> fields = Connection.GetTableFields("eleve");
-            //Console.WriteLine("ELEVE:");
-            //foreach (KeyValuePair<string, string> field in fields)
-            //{
-            //    Console.WriteLine(field.Key + " | " + field.Value);
-            //}
+            //description of student table
+            Dictionary<string, string> fields = Connection.GetTableFields("eleve");
+            Console.WriteLine("ELEVE:");
+            foreach (KeyValuePair<string, string> field in fields)
+            {
+                Console.WriteLine(field.Key + " | " + field.Value);
+            }
 
 
             //create student
-            Eleve elv1 = new Eleve();
-            elv1.id = 0;
-            elv1.prenom = "aymane";
-            elv1.nom = "dabdoubi";
-            elv1.code = "CB00000";
-            elv1.code_fil = "ginf2";
-            elv1.niveau = "YEET";
+            Eleve elv1 = new Eleve() { id = 2, code = "CB00000", nom = "dabdoubi", prenom = "aymane", code_fil = "ginf2", niveau = "TEEY" };
+            Eleve elv2 = new Eleve() { id = 2, code = "BC11111", nom = "oulad sine", prenom = "Saloua", code_fil = "ginf2", niveau = "TEEY" };
+            Eleve elv3 = new Eleve() { id = 2, code = "X010101", nom = "Ramy", prenom = "Ayman", code_fil = "ginf2", niveau = "YEET" };
+            Eleve elv4 = new Eleve() { id = 3, code = "X101010", nom = "benkhanous", prenom = "Salaheddine", code_fil = "ginf2", niveau ="YEET" };
 
-            Eleve elv2 = new Eleve();
-            elv2.id = 1;
-            elv2.prenom = "Saloua";
-            elv2.nom = "oulad sine";
-            elv2.code = "BC11111";
-            elv2.code_fil = "ginf2";
-            elv2.niveau = "TEEY";
+            //save student 
+            elv1.save();
+            Console.WriteLine($"student {elv1.code} saved");
 
-            Eleve elv3 = new Eleve();
-            elv3.id = 2;
-            elv3.prenom = "Ayman";
-            elv3.nom = "Ramy";
-            elv3.code = "XY010101";
-            elv3.code_fil = "ginf2";
-            elv3.niveau = "YEYE";
+            Console.WriteLine("\n find output:");
 
-            Eleve elv4 = new Eleve();
-            elv4.id = 3;
-            elv4.prenom = "Salaheddine";
-            elv4.nom = "benkhanous";
-            elv4.code = "YX101010";
-            elv4.code_fil = "ginf2";
-            elv4.niveau = "TETE";
+            //check if eleve exists in db
+            Eleve elvFound = (Eleve)elv1.find();
+            Console.WriteLine(elvFound == null ? "not found" : elvFound.ToString());
 
-            ////save student 
-            //elv1.save();
-            //Console.WriteLine($"student {elv1.code} saved");
+            //delete student
+            elv1.delete();
 
-            //Console.WriteLine("\n find output:");
+            Console.WriteLine("\n find output after delete:");
 
-            ////check if eleve exists in db
-            //Eleve elvFound = (Eleve)elv1.find();
-            //Console.WriteLine(elvFound == null ? "not found" : elvFound.ToString());
-
-            ////delete student
-            //elv1.delete();
-
-            //Console.WriteLine("\n find output after delete:");
-
-            ////check existance again
-            //elvFound = (Eleve)elv1.find();
-            //Console.WriteLine(elvFound == null ? "not found" : elvFound.ToString());
+            //check existance again
+            elvFound = (Eleve)elv1.find();
+            Console.WriteLine(elvFound == null ? "not found" : elvFound.ToString());
 
 
-            //Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("-----------------------------------------");
 
 
             //show all students
-            //elv1.save();
-            //elv2.save();
-            //elv3.save();
-            //elv4.save();
+            elv1.save();
+            elv2.save();
+            elv3.save();
+            elv4.save();
 
-            ////show all students
-            //Console.WriteLine($"All students:");
-            //List<dynamic> elvs = Eleve.All<Eleve>();
-            //foreach (Eleve eleve in elvs)
-            //{
-            //    Console.WriteLine(eleve.ToString());
-            //}
-
-
-            //Console.WriteLine("-----------------------------------------");
+            //show all students
+            Console.WriteLine($"All students:");
+            List<dynamic> elvs = Eleve.All<Eleve>();
+            foreach (Eleve eleve in elvs)
+            {
+                Console.WriteLine(eleve.ToString());
+            }
 
 
-            ////select using criteria
+            Console.WriteLine("-----------------------------------------");
+
+
+            //select using criteria
             Dictionary<string, object> criteria = new Dictionary<string, object>()
             {
                 {"niveau", "TEEY" }
